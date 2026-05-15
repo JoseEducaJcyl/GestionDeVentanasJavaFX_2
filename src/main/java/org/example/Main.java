423bfc4cec2263ea71fcabfd654b8aafcddfb939
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-05-15 10:24:09 +0200

    Ejercicio 20 del bloque 5 de programacion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,32 @@
 package org.example;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+public class Main extends Application {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Alerta Boton");
+        Button boton = new Button("Boton");
+        boton.setOnAction(e -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("ALERTA");
+            alert.setContentText("Has pulsado este boton y ha saltado esta alerta");
+            alert.showAndWait();
+        });
+
+        Scene escena = new Scene(boton,300,300);
+        stage.setScene(escena);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
